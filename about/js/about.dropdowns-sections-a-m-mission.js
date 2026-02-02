65534be8b3016a7fb468d3457b43d98051b153df
ruslansevastianov <ruslansevastianovdev@gmail.com>
2026-02-02 17:42:27 +0200

    add js for accordion section avout us mission page about

--- about/js/about.dropdowns-sections-a-m-mission.js
+++ about/js/about.dropdowns-sections-a-m-mission.js
@@ -1,2 +1,26 @@
-const button = document.querySelector(".a-m__accordion-container-button");
-console.log(button);
+const listButtonsDropdowns = document.querySelectorAll(".a-m__accordion-container-button");
+const contentDropdowns = document.querySelectorAll(".a-m__accordion-item-container-content");
+const listLineVerticalLines = document.querySelectorAll(".a-m__accordion-icon-line-vertical");
+
+for (let i = 0; i < listButtonsDropdowns.length; i++) {
+  listButtonsDropdowns[i].addEventListener("click", () => {
+    for (let j = 0; j < contentDropdowns.length; j++) {
+      if (j !== i) {
+        if (contentDropdowns[j].classList.contains("active-content-dropdown")) {
+          contentDropdowns[j].classList.remove("active-content-dropdown");
+          listLineVerticalLines[j].classList.remove("a-m__accordion-icon-line-vertical-not-visible");
+        } 
+      } else {
+        if (!contentDropdowns[i].classList.contains("active-content-dropdown")) {
+          contentDropdowns[i].classList.add("active-content-dropdown");
+           listLineVerticalLines[i].classList.add("a-m__accordion-icon-line-vertical-not-visible");
+        } else {
+          contentDropdowns[i].classList.remove("active-content-dropdown");
+        listLineVerticalLines[i].classList.remove("a-m__accordion-icon-line-vertical-not-visible");
+        }
+      }
+    }
+  });
+}
+
+
