1287dd77848aba903cbd2ab1000ebfba00ade84e
ruslansevastianov <ruslansevastianovdev@gmail.com>
2026-02-28 14:52:05 +0200

    adit base func cart modal

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,15 @@
 const cart = document.querySelector(".cart");
-const buttonCloseModal = document.querySelector(".cart__modal-container-icon-close");
-
+const cartModal = document.querySelector(".cart__modal");
+const buttonCloseModal = document.querySelector(
+  ".cart__modal-container-icon-close",
+);
 
 buttonCloseModal.addEventListener("click", () => {
-    cart.classList.add("not-visible");
-})
+  cart.classList.add("not-visible");
+});
 
+cart.addEventListener("click", (e) => {
+  if (!e.target.contains(cartModal)) {
+    cart.classList.add("not-visible");
+  }
+});
