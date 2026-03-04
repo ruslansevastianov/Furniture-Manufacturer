6a9e1b12df0f0cf83c15247fca88b8efacebc6bd
ruslansevastianov <ruslansevastianovdev@gmail.com>
2026-03-04 23:12:38 +0200

    add func products cart

--- js/cart.js
+++ js/cart.js
@@ -9,13 +9,13 @@ const buttonCloseModal = document.querySelector(
 
 buttonCloseModal.addEventListener("click", () => {
   cart.classList.add("not-visible");
-  document.body.classList.remove('no-scroll');
+  document.body.classList.remove("no-scroll");
 });
 
 fon.addEventListener("click", (e) => {
   if (!e.target.contains(cartModal)) {
     cart.classList.add("not-visible");
-    document.body.classList.remove('no-scroll');
+    document.body.classList.remove("no-scroll");
   }
 });
 
@@ -30,12 +30,18 @@ export class Cart {
     this.productsService = new ProductsService();
     this.cart = JSON.parse(localStorage.getItem("cart") ?? "{}");
 
+    this.containerPromoCodeTotal = document.querySelector(
+      ".cart__container-promo-code-total",
+    );
+
     this.renderCart();
   }
 
   async renderCart() {
     let quantityOfProducts = 0;
     let cartHtml = "";
+    let promoCodeTotal = "";
+    let total = 0;
 
     for (let count in this.cart) {
       quantityOfProducts += this.cart[count];
@@ -48,9 +54,84 @@ export class Cart {
         Number(productId),
       );
       cartHtml += this.createCartHtml(product);
+      total += this.cart[productId] * Number(product.price);
     }
 
+    promoCodeTotal = `
+        <div class = "cart__container-promo-code">
+          <a class = "cart__promo-code-header" href = "#">Promo code</a>
+          <div class = "cart__promo-code-container-input">
+            <input type = "text" class = "cart__promo-code-input" placeholder = "Enter promo code">
+            <button class = "cart__promo-code-button">OK</button>
+          </div>
+          <p class = "cart__promo-code-error-message cart__promo-code-error-message-not-visible">Promo code can't be applied</p>
+        </div>
+        <p class = "cart__total">Total: ${total.toFixed(2)}USD</p>
+    `;
+
     this.container.innerHTML = cartHtml;
+    this.containerPromoCodeTotal.innerHTML = promoCodeTotal;
+
+    this.container
+      .querySelectorAll(".cart__row-container-icon-del-product")
+      .forEach((element) => {
+        element.addEventListener("click", (e) => {
+          this.deleteProduct(e.target.dataset.id);
+        });
+      });
+
+    this.container
+      .querySelectorAll(".cart__row-triangle-up")
+      .forEach((element) => {
+        element.addEventListener("click", (e) => {
+          this.addProductOperation(e.target.dataset.id);
+        });
+      });
+
+    this.container
+      .querySelectorAll(".cart__row-triangle-down")
+      .forEach((element) => {
+        element.addEventListener("click", (e) => {
+          this.deleteProductOperation(e.target.dataset.id);
+        });
+      });
+
+    this.container.querySelectorAll(".cart__row-input").forEach((element) => {
+      element.addEventListener("input", (e) => {
+        if (e.target.value === "") {
+          element.value = 1;
+        }
+      });
+    });
+
+    this.containerPromoCodeTotal
+      .querySelector(".cart__promo-code-button")
+      .addEventListener("click", () => {
+        if (
+          this.containerPromoCodeTotal.querySelector(".cart__promo-code-input")
+            .value === ""
+        ) {
+          this.containerPromoCodeTotal
+            .querySelector(".cart__promo-code-input")
+            .classList.add("cart__promo-code-input-error");
+
+          this.containerPromoCodeTotal
+            .querySelector(".cart__promo-code-error-message")
+            .classList.remove("cart__promo-code-error-message-not-visible");
+        }
+      });
+
+    this.containerPromoCodeTotal
+      .querySelector(".cart__promo-code-input")
+      .addEventListener("input", (e) => {
+        if (e.target.value !== "") {
+          e.target.classList.remove("cart__promo-code-input-error");
+
+          this.containerPromoCodeTotal
+            .querySelector(".cart__promo-code-error-message")
+            .classList.add("cart__promo-code-error-message-not-visible");
+        }
+      });
   }
 
   createCartHtml(product) {
@@ -59,18 +140,20 @@ export class Cart {
     <div class = "cart__row-container-image">
       <img class = "cart__row-image" src= "${product.image}" alt = "${product.typeProduct}" >
     </div>
+    <div class = "cart__row-container-content">
     <a class = "cart__row-header" href = "#" >${product.typeProduct}</a>
     <div class = "cart__row-container-input">
       <input class = "cart__row-input" type="number" value = ${this.cart[product.id]}>
       <div class = "cart__row-container-triangles">
-        <div class="cart__row-triangle-up"></div>
-        <div class="cart__row-triangle-down"></div>
+        <div class="cart__row-triangle-up" data-id = "${product.id}"></div>
+        <div class="cart__row-triangle-down" data-id = "${product.id}"></div>
       </div>
     </div>
-    <div class = "cart__row-price">$${(Number(product.price)*this.cart[product.id]).toFixed(2)}USD</div>
-    <div class = "cart__row-container-icon-del-product">
-      <svg class = "cart__row-icon-del-product" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
-      <path class = "cart__row-icon-path-del-product" fill-rule="evenodd" clip-rule="evenodd" d="M10 2h3.6c.2 0 .4.2.4.4v1.2c0 .2-.2.4-.4.4H.4C.2 4 0 3.9 0 3.6V2.4c0-.2.2-.4.4-.3h3.7V2L4.9.3c.1-.2.2-.3.4-.3h3.5c.1 0 .3.1.4.2l.8 1.7V2zM1.8 16.1c.1 1 1 1.9 2 1.9h6.3c1.1 0 1.9-.8 2-1.9l1-11.1H1l.8 11.1zM12 6l-.8 10.1c0 .5-.5.9-1 .9H3.8c-.5 0-1-.4-1-.9L2 6h10zM5 8.1h1v6H5v-6zm4 0H8v6h1v-6z"></path>
+    <div class = "cart__row-price">$${(Number(product.price) * this.cart[product.id]).toFixed(2)}USD</div>
+    </div>
+    <div class = "cart__row-container-icon-del-product" data-id = "${product.id}">
+      <svg data-id = "${product.id}"  class = "cart__row-icon-del-product" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+      <path data-id = "${product.id}" class = "cart__row-icon-path-del-product" fill-rule="evenodd" clip-rule="evenodd" d="M10 2h3.6c.2 0 .4.2.4.4v1.2c0 .2-.2.4-.4.4H.4C.2 4 0 3.9 0 3.6V2.4c0-.2.2-.4.4-.3h3.7V2L4.9.3c.1-.2.2-.3.4-.3h3.5c.1 0 .3.1.4.2l.8 1.7V2zM1.8 16.1c.1 1 1 1.9 2 1.9h6.3c1.1 0 1.9-.8 2-1.9l1-11.1H1l.8 11.1zM12 6l-.8 10.1c0 .5-.5.9-1 .9H3.8c-.5 0-1-.4-1-.9L2 6h10zM5 8.1h1v6H5v-6zm4 0H8v6h1v-6z"></path>
       </svg>
     </div>
     </div>
@@ -79,6 +162,17 @@ export class Cart {
 
   addProductOperation(id) {
     this.cart[id] = (this.cart[id] || 0) + 1;
+    this.saveCart();
+    this.renderCart();
+  }
+  deleteProductOperation(id) {
+    if (this.cart[id] > 1) {
+      this.cart[id] -= 1;
+    } else {
+      this.cart[id] = 1;
+    }
+    this.saveCart();
+    this.renderCart();
   }
 
   saveCart() {
@@ -87,6 +181,16 @@ export class Cart {
 
   addProduct(id) {
     this.addProductOperation(id);
+    this.saveCart();
+    this.renderCart();
+  }
+
+  deleteProduct(id) {
+    for (const key in this.cart) {
+      if (key === id) {
+        delete this.cart[key];
+      }
+    }
     this.saveCart();
     this.renderCart();
   }
